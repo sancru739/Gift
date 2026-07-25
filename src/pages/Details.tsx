eb85b18149b67553ba8ea5b89cc6e7e1a647861a
SantiCrudo <santijcrudo@gmail.com>
2026-07-25 20:53:29 -0300

    Fix TS error unused import

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { motion } from "framer-motion"
 import DetailsGallery from "@/components/features/Details/DetailsGallery"
 import UploadModal from "@/components/features/Details/UploadModal"
